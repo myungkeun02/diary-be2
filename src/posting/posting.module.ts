0abcb91ed17059c086ae1fcbaed3f18a33b39fa9
최명근 <102522162+myungkeun02@users.noreply.github.com>
2023-06-07 22:40:46 +0900

    Revert "feat: 포스팅 구축 (1차)"

--- src/posting/posting.module.ts
+++ src/posting/posting.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PostingService } from './posting.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { Post } from 'src/models/post.model';
-import { PostingController } from './posting.controller';
 
 @Module({
-  imports: [
-    SequelizeModule.forFeature([Post])
-  ],
-  controllers: [PostingController],
   providers: [PostingService]
 })
 export class PostingModule {}
