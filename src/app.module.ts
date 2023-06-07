f753f24dab32c3c10d644809c37ec30168634d51
myungkeun <audrms3568@gmail.com>
2023-06-07 21:04:16 +0900

    feat: 포스팅 구축 (1차)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
 import { PostingController } from './posting/posting.controller';
 import { PostingModule } from './posting/posting.module';
 import { AuthModule } from './auth/auth.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { Post } from './models/post.model';
-import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    UserModule,
     PostingModule,
     AuthModule,
     ConfigModule.forRoot({
@@ -30,7 +25,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       models: [User, Post],
     }),
   ],
-  controllers: [AppController, UserController, PostingController],
-  providers: [AppService, UserService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/posting/posting.service.ts
+++ src/posting/posting.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Post } from '../models/post.model';
 
 @Injectable()
-export class PostingService {}
+export class PostingService {
+  constructor(
+    @InjectModel(Post)
+    private postModel: typeof Post,
+  ) {}
+
+  async writePost(title: string, content: string): Promise<Post> {
+    try {
+      const post = await this.postModel.create({
+        title: title,
+        content: content,
+      });
+      return post;
+    } catch (error) {
+      throw new Error('게시물 작성에 실패했습니다.');
+    }
+  }
+}
