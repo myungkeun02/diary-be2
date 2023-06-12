c021e2643465ecbc414a5d1426ee43ce7189e260
myungkeun <audrms3568@gmail.com>
2023-06-12 23:27:36 +0900

    feat: passport 구축(2차)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -7,42 +7,9 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @UseGuards(JwtAuthGuard)
   async login(@Request() req) {
-    const { username, password } = req.body;
-    const token = await this.authService.login(username, password);
-    return { token };
-  }
-
-  @Post('protected')
-  @UseGuards(AuthGuard('jwt'))
-  protectedRoute() {
-    // 이 경로는 JWT 인증으로 보호됩니다.
-    // 유효한 JWT로 인증된 요청만이 이 경로에 접근할 수 있습니다.
-    return '이는 보호된 경로입니다.';
-  }
+    console.log('Login!');
+    return req.user;
+  }ç
 }
-
-// import { Controller, Post, Body, UseGuards } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-
-// @Controller('auth')
-
-// export class AuthController {
-//   constructor(private authService: AuthService) {}
-
-//   @Post('login')
-//   async login(
-//     @Body('username') username: string,
-//     @Body('password') password: string,
-//   ) {
-//     const user = await this.authService.validateUser(username, password);
-//     if (user) {
-//       // 로그인 성공 처리
-//       const token = await this.authService.generateToken(user);
-//       return { token, message: '로그인 성공!' };
-//     } else {
-//       // 로그인 실패 처리
-//       return { message: '로그인 실패!' };
-//     }
-//   }
-// }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,75 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { User } from '../models/user.model';
+import { JwtService } from '@nestjs/jwt';
+import { hashSync, compareSync } from 'bcrypt';
+import { async } from 'rxjs';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly jwtService: JwtService,
+    @InjectModel(User)
     private userModel: typeof User,
+    private jwtService: JwtService,
   ) {}
 
-  async login(username: string, password: string) {
-    // 사용자명과 비밀번호를 유효성 검사합니다 (예: 데이터베이스 확인)
-    const isValid = await this.validateUser(username, password);
-    if (!isValid) {
-      throw new Error('유효하지 않은 사용자명 또는 비밀번호');
-    } else {
-      // JWT 토큰 생성
-      const payload = { username };
-      const token = this.jwtService.sign(payload);
-
-      return token;
-    }
-  }
-
-  async validateUser(username: string, password: string): Promise<User> {
-    const user = await this.userModel.findOne({ where: { username } });
-    if (user && (await this.verifyPassword(password, user.password))) {
-      return user;
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.findByLogin(username);
+    if (user && compareSync(password, user.password)) {
+      const { password, ...result } = user;
+      const accessToken = await this.jwtService.sign(result);
+      result['token'] = accessToken;
+      return result;
     }
     return null;
   }
-  async verifyPassword(
-    password: string,
-    hashedPassword: string,
-  ): Promise<boolean> {
-    // 비밀번호 검증 로직을 구현합니다. 여기서는 단순히 비밀번호가 일치하는지만 확인합니다.
-    return password === hashedPassword;
+
+  async findByLogin(username: string): Promise<User> {
+    const user = await this.userModel.findOne({ where: { username } });
+    if (!user) {
+      throw new ForbiddenException('아이디와 비밀번호를 다시 확인해주세요.');
+    }
+    return user;
   }
 }
-
-// import { Injectable } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/sequelize';
-// import { User } from '../models/user.model';
-// import { JwtService } from '@nestjs/jwt';
-
-// @Injectable()
-// export class AuthService {
-//   constructor(
-//     @InjectModel(User)
-//     private userModel: typeof User,
-//     private jwtService: JwtService,
-//   ) {}
-
-// async validateUser(username: string, password: string): Promise<User> {
-//   const user = await this.userModel.findOne({ where: { username } });
-//   if (user && user.password === password) {
-//     return user;
-//   }
-//   return null;
-// }
-
-//   async generateToken(user: User): Promise<string> {
-//     const payload = { username: user.username };
-//     return this.jwtService.sign(payload);
-//   }
-
-//   async validateToken(token: string): Promise<any> {
-//     try {
-//       return this.jwtService.verify(token);
-//     } catch (error) {
-//       throw new Error('Invalid token');
-//     }
-//   }
-// }
